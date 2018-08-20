26144db04554289b5456479477a6cf998cbe0377
AKing1998 <42119760+AKing1998@users.noreply.github.com>
2018-08-20 15:30:15 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-char email[100][1000]={0},word[100]={0};
+char email[2][10]={0},word[100]={0};
 int fun=0,decision;
 printf("Learning procedure is going to start....\n");
 for(int i=0;1==1;i++)
@@ -11,19 +11,18 @@ printf("Enter the %dth email: ",i);
 scanf("%s",&email[i]);
 
 //storing the words
-for(int j=0;j<1000;j++)
+for(int j=0,l=0;j<sizeof(email[i]);j++,l++)
 {
-for(int k=0;k<100;k++)
+for(int k=0;k<sizeof(word);k++)
 {if(email[i][j]==word[k])
 fun=1;
 
 if(fun!=1)
 word[j]=email[i][j];
-printf("%c\n",word[j]);
+printf("%c\n",word[l]);
 i=0;
 }
 }
-
 
 
 
