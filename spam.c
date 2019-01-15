c8faadcf396e56963e62d82bdd510f5214ff0a7a
Anandha Krishnan H <42119760+AKing1998@users.noreply.github.com>
2019-01-15 22:35:38 +0530

    Update spam.c

--- spam.c
+++ spam.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<string.h>
 //creartes a structure
-
-
 struct word 
 {
 char words[30];
@@ -10,7 +8,6 @@ int spam_word;
 int ham_word;
 float sbyw;
 };
-
 struct word_test //struct to store the testing values
 {
 	char words[30];
@@ -80,16 +77,10 @@ void main()
 			temp[temp_control]=lr[j];
 			temp_control=temp_control+1;
 			help:
-				printf("");
-			
-			
-			
-			
+				printf("");	
 		}		
-
 		memset(lr, '\0' , 100*sizeof(char));
 		i=i+1;
-
 		printf("\nEnter '1' to go to testing phase Enter '0' to continue learning\n");
 		scanf("%d",&br);
 		//if(br==1)//edit removed break statement since the while loop checks if br is one or not everytime
@@ -101,6 +92,8 @@ void main()
 //testing phase 
 //for loop required if we need to test multiple emails
 	//edit  
+
+
 	//this part is where we calculate and store the p(s/w) of all the words
 	
 	//to calculat the total number of spam words
@@ -116,14 +109,7 @@ void main()
 	//this is not p'(s/w) but just p(s/w) 
 	for(int j=0;learn[j].ham_word||learn[j].spam_word !=0;j=j+1)
 	{
-				
-		
-		
 		float n,wbys,wbyh,temp;
-		
-		
-		
-	
 		printf("\n%s\n",learn[j].words);
 	//0 might occur in the denominator and mess things up 
 	// i think that error is fixed but not too sure
@@ -131,13 +117,10 @@ void main()
 		wbyh=((float)learn[j].ham_word)/(float)total_ham;
 		learn[j].sbyw=(wbys/(wbys+wbyh));
 	  	printf("\n%d/%d=%f\n",learn[j].spam_word,total_spam,wbys);
-	  	printf("\n%d/%d=%f\n",learn[j].ham_word,total_ham,wbyh)
+	  	printf("\n%d/%d=%f\n",learn[j].ham_word,total_ham,wbyh);
 		printf("\nw/s=%f  w/h=%f  s/w=%f\n",wbys,wbyh,learn[j].sbyw);
-		
 	}
 	//edit
-	
-	
 	br=0;
 	while(br!=1)
 	{	
@@ -159,7 +142,6 @@ void main()
 //now we need to make that repeating words are not added to the struct 	
 		for(int j=0;ts[j]!= NULL ;j=j+1)//loop until full stop is observed
 		{
-		
 			if((int)ts[j]==32||(int)ts[j]==46) //ascii code for space=32 or fullstop=46
 			{	
 				for(int l=0;l<10;l=l+1)
@@ -192,11 +174,8 @@ void main()
 
 		memset(ts, '\0' , 100*sizeof(char));
 		i=i+1;
-
-		
 		//if(br==1)//edit removed break statement since the while loop checks if br is one or not everytime
 		//break;
-		
 		for(int m=0;m<test_control;m=m+1)
 		{
 			for(int n=0;n<100;n=n+1)
@@ -209,30 +188,56 @@ void main()
 			}
 			
 		}	
-		
 		for(int k=0;k<test_control;k=k+1)
 		printf("The words are:%s\nFrequency:%d\nSpam occurence:%d\nHam occurence:%d\n\n",test[k].words,test[k].freq,test[k].spamfreq,test[k].hamfreq);
 //edit
 		//Now for the main part as in calculating the probability of everything
+	float prob[1000]={0};
+	for(int j=0;test[j].hamfreq||test[j].spamfreq!=0;j=j+1)
+	{
+		float n,wbys,wbyh,temp,sbyw;
+		printf("\n%s\n",test[j].words);
+	//0 might occur in the denominator and mess things up 
+	// i think that error is fixed but not too sure
+		wbys=(((float)test[j].spamfreq)/(float)total_spam);
+		wbyh=((float)test[j].hamfreq)/(float)total_ham;
+		sbyw=(wbys/(wbys+wbyh));
+		prob[j]=(((float)3*(float)0.5)+((float)test[j].freq*(float)sbyw))/((float)3*(float)test[j].freq);
+		printf("..........%f %f ......",(float)test[j].freq*(float)sbyw,(float)3*(float)test[j].freq);
+	  	printf("\n%d/%d=%f\n",test[j].spamfreq,total_spam,wbys);
+	  	printf("\n%d/%d=%f\n",test[j].hamfreq,total_ham,wbyh);
+		printf("\nw/s=%f  w/h=%f  prob=%f\n",wbys,wbyh,prob[j]);
 		
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
+//edit	
+	//now for the part which calculates the actual probability if it is spam or not.
+	float final_prob=0;
+	for(int g=0;g<1000;g=g+1)
+	{
+		final_prob=final_prob+prob[g];
 		
+	}
+	float temp1=1;
+	float temp2=1;
+	for(int g=0;prob[g]!=0;g=g+1)
+	{
+		temp1=(float)temp1*(float)prob[g];
+	}
+	for(int g=0;prob[g]!=0;g=g+1)
+	{
+		temp1=(float)temp1*(1-(float)prob[g]);
+	}
+	
+		final_prob=(float)final_prob/((float)temp1+(float)temp2);
+	
+		if(final_prob<0.5)
+		printf("\nFinal Probability is %f -> This Message is HAM!\n",final_prob);
+		else
+		printf("\nFinal Probability is %f ->This Message is SPAM!\n",final_prob);
 		printf("\nDo you want to continue testing\nPress 1:STOP and Press 0:Continue testing\n");
 		scanf("%d",&br);	
 	}
-	
-	
-//edit
 }
-
 
 
 
